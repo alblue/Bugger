ba7d3f891bc37ab455593c467d2226f6096f7f32
Alex Blewitt <alex.blewitt@gmail.com>
2020-07-04 16:12:06 +0100

    Demonstration of bug with focus on NSApplication

---
+++ Bugger/main.m
@@ -0,0 +1,49 @@
+//
+//  main.m
+//  Bugger
+//
+//  Created by Alex Blewitt on 04/07/2020.
+//  Copyright © 2020 Me. All rights reserved.
+//
+//
+// Taken from https://bugs.eclipse.org/bugs/show_bug.cgi?id=562535#c0
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject<NSApplicationDelegate>
+{
+}
+@end
+
+@implementation AppDelegate : NSObject
+-(BOOL) applicationShouldTerminateAfterLastWindowClosed:(NSApplication*)app
+{
+    return YES;
+}
+@end
+
+int main ()
+{
+    NSApplication* app = [NSApplication sharedApplication];
+    AppDelegate* appDelegate = [[AppDelegate alloc] init];
+    [app setDelegate:appDelegate];
+    [app setActivationPolicy:NSApplicationActivationPolicyRegular];
+
+	NSMenuItem* item = NSMenuItem.new;
+	NSApp.mainMenu = NSMenu.new;
+	item.submenu = NSMenu.new;
+	[app.mainMenu addItem: item];
+	[item.submenu addItem: [[NSMenuItem alloc] initWithTitle:@"Quit" action:@selector(terminate:) keyEquivalent:@"q"]];
+	
+    NSRect windowRect = NSMakeRect(0, 0, 200, 200);
+    id window = [[NSWindow alloc]
+        initWithContentRect: windowRect
+        styleMask: NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskResizable | NSWindowStyleMaskMiniaturizable
+        backing: NSBackingStoreBuffered
+        defer:NO
+        ];
+    [window setTitle:[[NSProcessInfo processInfo] processName]];
+    [window makeKeyAndOrderFront:nil];
+    [app run];
+    return 0;
+}
