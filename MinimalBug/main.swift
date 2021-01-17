def736d6eb46df395ab8313a8d84446b0344b71c
Alex Blewitt <alex.blewitt@gmail.com>
2021-01-17 16:38:31 +0000

    Fix for menu on macOS systems

--- MinimalBug/main.swift
+++ MinimalBug/main.swift
@@ -3,8 +3,15 @@
 // to another application and back again
 import AppKit
 
+@objc
+class Delegate: NSObject, NSApplicationDelegate {
+	func applicationDidFinishLaunching(_ notification: Notification) {
+		app.activate(ignoringOtherApps: true)
+	}
+}
+
 var app = NSApplication.shared
 app.setActivationPolicy(.regular)
-
-app.activate(ignoringOtherApps: true)
+let delegate = Delegate()
+app.delegate = delegate
 app.run()
